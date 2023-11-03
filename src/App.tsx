4b5fb5a2d33bbf20fae394705df096cccd1d561b
Nicolas Demianiw <ndemia@fastmail.com>
2023-11-03 16:49:20 +0100

    Cleanup and include TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import { useState } from 'react';
-import './App.css';
+import {} from 'react';
 
-function App() {
-	const [count, setCount] = useState(0);
-
+export default function App() {
 	return (
 		<>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+			<h1 className="text-5xl underline">Click on the Vite and React logos to learn more</h1>
 		</>
 	);
 }
-
-export default App;
