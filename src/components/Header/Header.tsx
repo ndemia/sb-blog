597ef3479681f7a8c26ad377c988164bceb3e1eb
Nicolas Demianiw <ndemia@fastmail.com>
2023-11-08 14:46:10 +0100

    Add styles to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Heading from "../Heading/Heading";
 const Header = () => {
   let page = "Home";
   return (
-    <header className="flex h-12 flex-col bg-slate-700 bg-[url('/assets/images/banner.jpg')]">
+    <header className="flex h-12 flex-col items-center bg-[url('/assets/images/banner.jpg')] bg-cover bg-no-repeat py-20">
       <Heading title={page}></Heading>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Metropolis", "Figtree", "Montserrat", "sans-serif"],
+      },
+      brightness: {
+        70: ".7",
+      },
+    },
+  },
+  plugins: [],
 };
