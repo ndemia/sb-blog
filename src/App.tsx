b5fe9e1928a9b926832854200affff1e452cb019
Nicolas Demianiw <ndemia@fastmail.com>
2023-11-10 17:14:21 +0100

    Create basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import Header from './components/Header/Header';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Blog from "./pages/Blog/Blog";
+import Header from "./components/Header/Header";
 
 const App = () => {
-	return (
-		<>
-			<Header />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
