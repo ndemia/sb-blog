0efcc50d80801ea302430aa02520ee4c093086e7
Nicolas Demianiw <ndemia@fastmail.com>
2023-12-06 17:08:03 +0100

    Create new interfaces

--- src/utilities/interfaces.tsx
+++ src/utilities/interfaces.tsx
@@ -12,3 +12,40 @@ export interface CategoryInterface {
   created_at: number | null;
   updated_at: number | null;
 }
+
+export interface BlogpostInterface {
+  category: CategoryInterface;
+  category_id: number;
+  content: string;
+  created_at: string;
+  id: number;
+  img_url: string;
+  title: string;
+  updated_at: string;
+}
+
+export interface BlogpostsResponseInterface {
+  current_page: number;
+  data: BlogpostInterface[];
+  first_page_url: string;
+  from: number;
+  last_page: number;
+  last_page_url: string;
+  links: {}[];
+  next_page_url: string;
+  path: string;
+  per_page: number | string;
+  prev_page_url: string | null;
+  to: number;
+  total: number;
+}
+
+export interface FetchReturnInterface {
+  data: CategoryInterface[] | BlogpostsResponseInterface[];
+  isPending: boolean;
+  error: string | null;
+}
+
+export interface BlogpostCardProps {
+  blogpostData: BlogpostInterface;
+}
