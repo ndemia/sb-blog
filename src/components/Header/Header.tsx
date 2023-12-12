7250070dfa7a696cfa7f7936732d8d759952e8df
Nicolas Demianiw <ndemia@fastmail.com>
2023-12-12 16:21:59 +0100

    Change hero image and adjust position as necessary

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Heading from "../Heading/Heading";
 const Header = () => {
   let page = "Home";
   return (
-    <header className="flex flex-col items-center bg-[url('/assets/images/banner.jpg')] bg-cover bg-no-repeat py-8">
+    <header className="flex flex-col items-center bg-[url('/assets/images/banner.jpg')] bg-cover bg-center bg-no-repeat py-8">
       <Navigation />
       <Heading title={page}></Heading>
     </header>
