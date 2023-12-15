66767e5a537ca1cf081962560ae2baff64363167
Nicolas Demianiw <ndemia@fastmail.com>
2023-12-15 10:19:37 +0100

    Add container to Header in order to limit its width

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,10 @@ const Header = () => {
   let page = "Home";
   return (
     <header className="flex flex-col items-center bg-[url('/assets/images/banner.jpg')] bg-cover bg-center bg-no-repeat py-8">
-      <Navigation />
-      <Heading title={page}></Heading>
+      <div className="w-full max-w-[1024px]">
+        <Navigation />
+        <Heading title={page}></Heading>
+      </div>
     </header>
   );
 };
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -5,7 +5,11 @@ interface HeadingProps {
 }
 
 const Heading = ({ title }: HeadingProps): ReactElement => {
-  return <h1 className="py-8 text-5xl font-bold text-white">{title}</h1>;
+  return (
+    <h1 className="py-8 text-center text-5xl font-bold text-sb-white">
+      {title}
+    </h1>
+  );
 };
 
 export default Heading;
